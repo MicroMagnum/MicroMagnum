b228782adad90d69127eb898aa3b80fac8879ba3
Gunnar Selke <gselke@physnet.uni-hamburg.de>
2013-03-13 17:12:08 +0100

    more SP1

--- examples/sp1/sp1.py
+++ examples/sp1/sp1.py
@@ -3,28 +3,23 @@ from magnum import *
 from frange import frange
 from math import cos, sin, pi
 
-#print "TODO: Implement SP1 :)"; import sys; sys.exit()
-
-#mesh = RectangularMesh((100,50,1), (20e-9, 20e-9, 20e-9))
-mesh = RectangularMesh((50,25,1), (40e-9, 40e-9, 20e-9))
+#mesh = RectangularMesh((50,100,1), (20e-9, 20e-9, 20e-9))
+mesh = RectangularMesh((25,50,1), (40e-9, 40e-9, 20e-9))
 Py = Material.Py(k_uniaxial=5e2, axis1=(1,0,0))
 world = World(mesh, Body("square", Py, Everywhere()))
 
 solver = create_solver(world, [StrayField, ExchangeField, AnisotropyField, ExternalField], log=True)
-
-# Create initial state
 solver.state.M = (8e5, 0, 0)
 
 # Perform hysteresis
 H_range = list(frange(+50e-3, -50e-3, -5e-3)) + list(frange(-50e-3, +50e-3, 5e-3))
 H_range = [H/MU0 for H in H_range] # convert Tesla->A/m
-#print H_range
 
 def hysteresis(axis):
   if axis == "long":
-    ax = (cos(pi/180), sin(pi/180), 0.0)
+    ax = (sin(pi/180), cos(pi/180), 0.0) # along 'long' y axis
   elif axis == "short":
-    ax = (sin(pi/180), cos(pi/180), 0.0)
+    ax = (cos(pi/180), sin(pi/180), 0.0) # along 'short' x axis
   else:
     raise ValueError("need to specify 'long' or 'short'")
 
@@ -43,4 +38,3 @@ def hysteresis(axis):
 
 hysteresis("long")
 hysteresis("short")
-
