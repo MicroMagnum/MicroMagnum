3bd7b56f47a7d4598333a0b47f4c882c3396ab61
Gunnar Selke <gselke@physnet.uni-hamburg.de>
2013-03-13 15:43:24 +0100

    small changes

--- examples/sp3/sp3.py
+++ examples/sp3/sp3.py
@@ -11,7 +11,7 @@ l_ex = math.sqrt(Py.A/K_m)
 
 # 8.47, 8.52
 L = 8.52*l_ex # cube side length
-n = 24 # number of cells in each direction
+n = 16 # number of cells in each direction
 world = World(RectangularMesh((n,n,n),(L/n,L/n,L/n)), Body("cube", Py))
 
 solver = create_solver(world, [StrayField, ExchangeField, AnisotropyField], log=True, do_precess=False)
@@ -21,8 +21,7 @@ def get_energies(state):
   E_exch  = state.E_exch  / K_m / L**3
   E_aniso = state.E_aniso / K_m / L**3
   E_tot = E_stray + E_exch + E_aniso
-  #return tuple(map(lambda x: x/Py.Ms, state.M.average()))
-  return [E_stray, E_exch, E_aniso], E_tot, map(lambda x: x/Py.Ms, state.M.average())
+  return E_tot, [E_stray, E_exch, E_aniso], [x/Py.Ms for x in state.M.average()]
 
 def my_vortex(field, pos): 
   chirality = 1.0
@@ -37,12 +36,13 @@ def my_vortex(field, pos):
   return Mx*scale, My*scale, Mz*scale
 
 solver.state.M = my_vortex
-#print solver.state.M.average()
 solver.relax(2)
+writeOMF("state-vortex.omf", solver.state.M)
 E_vortex = ("vortex",) + get_energies(solver.state)
 
 solver.state.M = (0,0,Py.Ms)
 solver.relax(2)
+writeOMF("state-flower.omf", solver.state.M)
 E_flower = ("flower",) + get_energies(solver.state)
 
 print E_flower
