5064f8a3186c1f339f2782605a606b598f2ec0e3
Gunnar Selke <gselke@physnet.uni-hamburg.de>
2013-03-22 13:55:54 +0100

    Finished SP2 script. Now SP1 to SP4 work correctly.

--- examples/sp2/sp2.py
+++ examples/sp2/sp2.py
@@ -2,12 +2,11 @@
 from magnum import *
 import math
 
-# Material parameters
+# material parameters
 Py = Material.Py()
-A = Py.A
 Ms = Py.Ms
 K_m = 0.5*MU0*(Ms*Ms)
-l_ex = math.sqrt(A/K_m)
+l_ex = math.sqrt(Py.A/K_m)
 
 def geometry(ratio):
   d = ratio * l_ex
@@ -22,37 +21,44 @@ def discretize(L, d, t):
   nx, dx = choose(L)
   ny, dy = choose(d)
   nz, dz = choose(t)
+  assert dx < l_ex and dy < l_ex and dz < l_ex
   return RectangularMesh((nx, ny, nz), (dx, dy, dz))
 
 def field(A, axis = (1,1,1)):
-  return tuple(A * axis[i] / (axis[0]**2 + axis[1]**2 + axis[2]**2) for i in (0,1,2))
+  return tuple(A * axis[i] / math.sqrt(axis[0]**2 + axis[1]**2 + axis[2]**2) for i in (0,1,2))
 
 for ratio in range(1,40+1):
   L, d, t = geometry(ratio)
   print "d/l_ex=%s, L=%s, d=%s, t=%s" % (ratio, L, d, t)
 
   mesh = discretize(L, d, t)
-  print "nn:", mesh.num_nodes
-  print "cells:", mesh.delta
-  
   world = World(mesh, Body("thinfilm", Py, Everywhere()))
   solver = create_solver(world, [StrayField, ExchangeField, ExternalField], log=False, do_precess=False)
   solver.state.M = (Ms,0.0,0.0)
 
-  # do hysteresis
+  # 1. do hysteresis
   H = 10.0e-3/MU0
   while True:
     solver.state.H_ext_offs = field(H, (1,1,1))
     solver.relax(1.0)
 
-    h = H/Py.Ms
-    m = tuple(a/Py.Ms for a in solver.state.M.average())
-    
-    print h, m
-    if abs(H) < 1e-10:
-      f = open("sp2.txt", "a")
-      f.write("%s    %s %s %s\n" % (ratio, m[0], m[1], m[2]))
-      f.close()
-      break
+    h = H/Ms
+    m = tuple(a/Ms for a in solver.state.M.average())
+    print h, m, sum(m)*Ms
 
+    # remember remanence mag
+    if abs(h) < 1e-10:
+      m_rem = m
+
+    # find H_coerc
+    if h < 0 and sum(m) < 1000/Ms:
+      h_coerc = abs(h)
+      break # exit hysteresis loop
+
+    # next hysteresis step
     H -= 0.1e-3/MU0
+
+  # 2. log results
+  f = open("sp2.txt", "a")
+  f.write("%s    %s    %s %s %s\n" % (ratio, h_coerc, m_rem[0], m_rem[1], m_rem[2]))
+  f.close()
