fc6188e05e0a01c365d1486562745c70293707b0
Gunnar Selke <gunnar@gmx.info>
2013-04-01 21:57:25 +0200

    v0.2.0rc2

--- src/magnum/__init__.py
+++ src/magnum/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with MicroMagnum.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.2.0rc"
+__version__ = "0.2.0rc2"
 
 # I. Import extension lib
 from . import magneto
