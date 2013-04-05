3ad3bc72cd2aa001b5319df5585c7695d4d3afdb
Christian <sdf@lavabit.com>
2013-04-05 14:37:27 +0200

    evolver init

--- src/magnum/evolver/__init__.py
+++ src/magnum/evolver/__init__.py
@@ -20,9 +20,10 @@ from .evolver import Evolver # base class
 from .euler import Euler
 from .runge_kutta import RungeKutta
 from .runge_kutta_4 import RungeKutta4
+from .cvode import Cvode
 from .stepsize_controller import StepSizeController, NRStepSizeController, FixedStepSizeController
 
 # evolver state class
 from .state import State
 
-__all__ = ["Evolver", "Euler", "RungeKutta", "RungeKutta4", "StepSizeController", "NRStepSizeController", "FixedStepSizeController", "State"]
+__all__ = ["Evolver", "Euler", "RungeKutta", "RungeKutta4", "Cvode", "StepSizeController", "NRStepSizeController", "FixedStepSizeController", "State"]
