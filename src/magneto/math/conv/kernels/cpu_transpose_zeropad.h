7ca3d5a242e8caf692125fe943772ec8b538c10c
Gunnar Selke <gunnar@gnnr.de>
2013-04-11 15:33:55 +0200

    fixed typo in comment

--- src/magneto/math/conv/kernels/cpu_transpose_zeropad.h
+++ src/magneto/math/conv/kernels/cpu_transpose_zeropad.h
@@ -22,7 +22,7 @@
 
 void cpu_transpose_zeropad_c2c(
 	int dim_x, int dim_y, int dim_z, // input size
-	int exp_y, // exp_x >= dim_x
+	int exp_y, // exp_y >= dim_x
 	const double  *in_x, const double  *in_y, const double  *in_z,   // size: dim_x * dim_y * dim_z
 	      double *out_x,       double *out_y,       double *out_z);  // size: exp_y * dim_z * dim_x
 
