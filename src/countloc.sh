22c105f2e4ffa701c0e1a29d5a4daecf56b4b6cf
Gunnar Selke <gunnar@gmx.info>
2013-04-23 23:55:19 +0200

    don't count locs in magnum/external dir

--- src/countloc.sh
+++ src/countloc.sh
@@ -1,4 +1,4 @@
-files=`find . -name \*.h -or -name \*.cpp -or -name \*.i -or -name \*.py -or -name \*.cu | grep -v "magneto_cuda.py" - | grep -v "magneto_cpu.py" - | grep -v "build/" -`
+files=`find . -name \*.h -or -name \*.cpp -or -name \*.i -or -name \*.py -or -name \*.cu | grep -v "magneto_cuda.py" - | grep -v "magneto_cpu.py" - | grep -v "build/" - | grep -v "magnum.external/"`
 wcinfo=`cat $files | sed '/^\s*$/d' | wc`
 loc=`echo $wcinfo | awk '{print $1}'`
 bytes=`echo $wcinfo | awk '{print $3}'`
